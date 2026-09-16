26e77ebb8bbd6c27884a5ae0b46b614e6330f493
mi-demira <pijolina1@gmail.com>
2026-09-16 19:33:10 +0300

    Add files via upload

---
+++ kattis- bela.cpp
@@ -0,0 +1,63 @@
+﻿using namespace std;
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+	//kolko ruce ima v igrata
+	int N;
+	cin >> N;
+
+	//boqta koqto e koz
+	char dominant;
+	cin >> dominant;
+
+	int points = 0;
+
+	//input cards
+	for (int i = 1; i <= N * 4; i++)
+	{
+		string card;
+		cin >> card;
+
+		if (card[1] == dominant)
+		{
+			if (card[0] == 'J')
+			{
+				points += 20;
+			}
+			else if (card[0] == '9')
+			{
+				points += 14;
+			}
+		}
+		else
+		{
+
+			if (card[0] == 'J')
+			{
+				points += 2;
+			}
+			
+		}
+		
+		switch (card[0])
+		{
+		case 'A':
+			points += 11;
+			break;
+		case 'K':
+			points += 4;
+			break;
+		case 'Q':
+			points += 3;
+			break;
+		case 'T':
+			points += 10;
+			break;
+		}
+	}
+
+	cout << points << endl;
+}
